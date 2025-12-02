e106d98f2107801cc0e8ab8e953432c870ce6402
nnnMUT <6811130002@mut.ac.th>
2025-12-02 17:30:06 +0700

    hi

--- LAB3/lAB03.cpp
+++ LAB3/lAB03.cpp
@@ -1,20 +1,53 @@
-// lAB03.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+﻿#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	string name;
+	int age;
+	string stuID;
+	int score;
+	string grade;
+
+	cout << "Enter name : ";
+	cin >> name;
+	cout << "Enter Age : ";
+	cin >> age;
+	cout << "Enter Student ID : ";
+	cin >> stuID;
+	cout << "Enter score : ";
+	cin >> score ;
+
+	if (score >= 90) 
+	{
+		grade = "เกรด A" ;
+	}
+	else if (score >= 80) 
+	{
+		grade = "เกรด B" ;
+	}
+	else if (score >= 70)
+	{
+		grade = "เกรด C" ;
+	}
+	else 
+	{
+		grade = "เกรด F" ;
+	}
+	
+  cout << "Student Report : " << endl;
+  cout << "Name: " << name << endl;
+  cout << "Age: " << age << endl;
+  cout << "Student ID: " << stuID << endl;
+  cout << "Grade: " <<grade << endl;
+  cout << "------------------"<< endl;
+
+  return 0;
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
